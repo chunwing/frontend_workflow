d5ea7285169074d305bb882a6736f828fc3a8f27
chunwing <mr.ncwing@gmail.com>
2016-06-28 18:21:00 +0800

    feature/update-build-task

---
+++ files/js/index.js
@@ -0,0 +1,9 @@
+<!-- build:css -->
+// inject:app
+// endinject
+<!-- endbuild -->//
+require('./public/global');
+$(document).ready(function(){
+    console.log('Here is Index');
+    console.log('Here is Index');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     browserify = require('browserify'),
     streamify = require('gulp-streamify'),
-
     glob = require('glob'),
     buffer = require('vinyl-buffer'),
     es = require('event-stream'),
@@ -14,25 +13,25 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     htmlmin = require('gulp-htmlmin'),
     imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
     sourcemaps = require('gulp-sourcemaps'),
     watchify = require('watchify'),
+    autoprefixer = require('autoprefixer'),
+    postcss      = require('gulp-postcss'),
     browserSync = require('browser-sync').create(),
     reload = browserSync.reload;
-
-
-
-gulp.task('dev', ['injectsass', 'html:watch'], function(done){
-
+gulp.task('dev', ['injectsass', 'html'], function(done){
     glob('files/js/*.js', function(err, files){
         if (err) done(err);
         var tasks = files.map( function(entry){
-
             gulp.src(entry)
                 .pipe(htmlreplace({
-                    'css': ''
-                }))
+                    'css': '\n// inject:app\n// endinject\n'
+                }, {
+                    keepBlockTags: true
+                }
+                ))
                 .pipe(gulp.dest('files/js'));
-
             var b = browserify({
                 entries: [entry],
                 extensions: ['.js'],
@@ -41,55 +40,47 @@ gulp.task('dev', ['injectsass', 'html:watch'], function(done){
                 packageCache: {},
                 fullPaths: true
             })
-                .plugin(watchify);
-
-            var bundle = function() {
-                return b.bundle()
-                    .on('error', function(e){
-                        gutil.log(e);
-                    })
-                    .pipe(source(entry))
-                    .pipe(buffer())
-                    .pipe(gulp.dest('dist'))
-                    .pipe(browserSync.stream());
-            };
-
-            b.on('update', bundle);
-            b.on('log',gutil.log);
-            return bundle();
+            .plugin(watchify);
+        var bundle = function() {
+            return b.bundle()
+                .on('error', function(e){
+                    gutil.log(e);
+                })
+                .pipe(source(entry))
+                .pipe(buffer())
+                .pipe(gulp.dest('dist'))
+                .pipe(browserSync.stream());
+        };
+        b.on('update', bundle);
+        b.on('log',gutil.log);
+        return bundle();
         });
         es.merge(tasks).on('end', done);
     });
-
-
     browserSync.init({
         injectChanges: true,
+        port: 3014,
         server:{ 
             baseDir:  "./",
             routes: {
                 "/files": "files"
             }, 
-            index: "views/t1_.html"
+            index: "Views/index.html"
         },
         logFileChanges: false
     });
-
 });
-
 gulp.task('injectsass', ['sass:watch'], function(done){
     var injectAppFiles = gulp.src('files/sass/public/*.scss', {read: false});
-
     function transformFilepath(filepath) {
         return '@import "' + filepath + '";';
     }
-
     var injectAppOptions = {
         transform: transformFilepath,
         starttag: '// inject:app',
         endtag: '// endinject',
         addRootSlash: false
     };
-
     glob('files/sass/*.scss', function(err, files){
         if (err) done(err);
         var tasks = files.map( function(entry){
@@ -97,50 +88,91 @@ gulp.task('injectsass', ['sass:watch'], function(done){
                 .pipe(inject(injectAppFiles, injectAppOptions))
                 .pipe(sourcemaps.init())
                 .pipe(sass().on('error', sass.logError))
+                .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
                 .pipe(sourcemaps.write('./maps'))
                 .pipe(gulp.dest('files/css'))
                 .pipe(browserSync.stream({match: '**/*.css'}));
-
         });
         es.merge(tasks).on('end', done);
     });
 });
+gulp.task('build', ['injectcss', 'html', 'image'], function(done){
+    glob('files/js/*.js', function(err, files){
+        if (err) done(err);
+        var tasks = files.map( function(entry){
+            var b = browserify({
+                entries: [entry],
+                extensions: ['.js'],
+                debug: true,
+                cache: {},
+                packageCache: {},
+                fullPaths: true
+            })
+            .transform(require('browserify-css'), {
+                rootDir: '.',
 
+                autoInject: 'true',
+                minify: 'true'
+            })
+            .plugin(watchify);
+        var bundle = function() {
+            return b.bundle()
+                .on('error', function(e){
+                    gutil.log(e);
+                })
+                .pipe(source(entry))
+                .pipe(streamify(uglify()))
+                .pipe(buffer())
+                .pipe(gulp.dest('dist'))
+                .pipe(browserSync.stream());
+        };
+        b.on('update', bundle);
+        b.on('log',gutil.log);
+        return bundle();
+        });
+        es.merge(tasks).on('end', done);
+    });
+    browserSync.init({
+        injectChanges: true,
+        port: 3015,
+        server:{ 
+            baseDir:  "dist",
+            routes: {
+                "/files": "files"
+            }, 
+            index: "Views/index.html"
+        },
+        logFileChanges: false
+    });
+});
 gulp.task('injectcss', function(done){
-
     function transformFilepath(filepath) {
         return "require('" + filepath + "');";
     }
-
     var injectAppOptions = {
         transform: transformFilepath,
         starttag: '// inject:app',
         endtag: '// endinject',
         addRootSlash: false
     };
-
     glob('files/js/*.js', function(err, files){
         if (err) done(err);
         var tasks = files.map( function(entry){
-
             var injectAppFiles = gulp.src(entry, { base: process.cwd() })
                 .pipe(rename({
                     dirname: "../css",
                     extname: ".css"
                 }));
-
             gulp.src(entry)
                 .pipe(inject(injectAppFiles, injectAppOptions))
                 .pipe(gulp.dest('files/js'))
                 .pipe(browserSync.stream({match: '**/*.js'}));
-
             var b = browserify(entry)
                 .transform(require('browserify-css'), {
                     rootDir: '.',
                     autoInject: 'true',
                     minify: 'true'
                 });
-
             function bundle (bundler) {   
                 return bundler
                     .bundle()
@@ -151,90 +183,44 @@ gulp.task('injectcss', function(done){
                     .pipe(gulp.dest('dist'))
                     .pipe(browserSync.stream());
             }
-
             return bundle(b);
         });
         es.merge(tasks).on('end', done);
     });
 });
-
-gulp.task('build', ['sass', 'html', 'image'], function(){
-    function bundle (bundler) {   
-        return bundler
-            .bundle()
-            .on('error', function(e){
-                gutil.log(e);
-            })
-            .pipe(source('bundle.js'))
-            .pipe(streamify(sourcemaps.init()))
-            .pipe(streamify(uglify()))
-            .pipe(streamify(sourcemaps.write('./maps')))
-            .pipe(gulp.dest('dist/files/js'))
-            .pipe(browserSync.stream());
-    }
-
-    var watcher = watchify( 
-        browserify('./files/js/t1.js', watchify.args)
-        .transform(require('browserify-css'), {
-            rootDir: '.',
-            autoInject: 'true',
-            minify: 'true'
-        })
-    );
-
-    bundle(watcher);
-    watcher.on('update', function(){
-        bundle(watcher);
-    });
-    watcher.on('log',gutil.log);
-
-    browserSync.init({
-        server:{ 
-            baseDir:  "dist",
-            routes: {
-                "/files": "files"
-            }, 
-            index: "views/index.html"
-        },
-        logFileChanges: false
-    });
-});
-
-
-
-/********** compile sass **********/
-gulp.task('sass', ['sass:watch'], function () {
-    return gulp.src('files/sass/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('files/css'));
-});
-
 /********** minify html **********/
 gulp.task('html', ['html:watch'], function() {
-    return gulp.src('./views/*.html')
+    return gulp.src('./Views/*.html')
+        .pipe(htmlreplace({
+            'css': '',
+            'js' : {
+                src: '../files/js',
+                tpl: '<script src="%s/%f.js"></script>'
+            }
+        }, {
+            keepBlockTags: true
+        }
+        ))
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist/views'));
+        .pipe(gulp.dest('dist/Views'));
 });
-
 /********** minify image **********/
 gulp.task('image', function() {
     gulp.src('./files/img/*')
-        .pipe(imagemin())
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
         .pipe(gulp.dest('dist/files/img'));
 });
-
 /********** watch sass **********/
 gulp.task('sass:watch', function () {
     gulp.watch('files/sass/*.scss', ['injectsass'])
         .on('log', gutil.log);
 });
-
 /********** watch html **********/
 gulp.task('html:watch', function () {
-    gulp.watch('views/*.html', ['html'])
+    gulp.watch('Views/*.html', ['html'])
         .on('log', gutil.log);
     reload();
 });
-
